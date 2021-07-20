83a41f3519465d2d60a3a19d3077658ea3c9d251
adheli.tavares <>
2021-07-20 20:22:27 +0100

    Initial commit

---
+++ src/main/java/com/tavares/messeger/MessegerApplication.java
@@ -0,0 +1,13 @@
+package com.tavares.messeger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tavares/messeger/MessegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tavares.messeger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
